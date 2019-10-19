3ce78d6642f6e1192fc684a07fb3ca914c4f1487
allisonkydy <allisnelly@gmail.com>
2019-10-19 18:02:49 -0500

    renamed directories, packages, file where appropriate

---
+++ medcabinet/src/main/java/com/lambdaschool/medcabinet/repository/UseremailRepository.java
@@ -0,0 +1,11 @@
+package com.lambdaschool.medcabinet.repository;
+
+import com.lambdaschool.medcabinet.models.Useremail;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UseremailRepository extends CrudRepository<Useremail, Long>
+{
+    List<Useremail> findAllByUser_Username(String name);
+}
